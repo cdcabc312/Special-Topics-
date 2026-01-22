2c199ba72815ef9d8dc84ed2752abd5271286ea4
Colin Carta <cdcabc1234@gmail.com>
2026-01-21 20:23:05 -0500

    Hello World portion of lab complete, need to finish other 2 parts

---
+++ Lab 1/Lab_1/main/hello_world_main.c
@@ -0,0 +1,72 @@
+/*
+ * SPDX-FileCopyrightText: 2010-2022 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: CC0-1.0
+ */
+
+#include <stdio.h>
+#include <inttypes.h>
+#include "sdkconfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_chip_info.h"
+#include "esp_flash.h"
+#include "esp_system.h"
+#include "esp_timer.h"
+
+#include "driver/gpio.h"
+#include "esp_log.h"
+
+#define LED_GPIO GPIO_NUM_2
+static const char *TAG = "LED";
+
+void app_main(void)
+{
+    printf("Hello world!\n");
+
+    /* Print chip information */
+    esp_chip_info_t chip_info;
+    uint32_t flash_size;
+    esp_chip_info(&chip_info);
+    printf("This is %s chip with %d CPU core(s), %s%s%s%s, ",
+           CONFIG_IDF_TARGET,
+           chip_info.cores,
+           (chip_info.features & CHIP_FEATURE_WIFI_BGN) ? "WiFi/" : "",
+           (chip_info.features & CHIP_FEATURE_BT) ? "BT" : "",
+           (chip_info.features & CHIP_FEATURE_BLE) ? "BLE" : "",
+           (chip_info.features & CHIP_FEATURE_IEEE802154) ? ", 802.15.4 (Zigbee/Thread)" : "");
+
+    unsigned major_rev = chip_info.revision / 100;
+    unsigned minor_rev = chip_info.revision % 100;
+    printf("silicon revision v%d.%d, ", major_rev, minor_rev);
+    if (esp_flash_get_size(NULL, &flash_size) != ESP_OK) {
+        printf("Get flash size failed\n");
+        return;
+    }
+
+    printf("%" PRIu32 "MB %s flash\n", flash_size / (uint32_t)(1024 * 1024),
+           (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
+
+    printf("Minimum free heap size: %" PRIu32 " bytes\n", esp_get_minimum_free_heap_size());
+
+    /* Configure LED GPIO */
+    gpio_reset_pin(LED_GPIO);
+    gpio_set_direction(LED_GPIO, GPIO_MODE_OUTPUT);
+
+    int led_state = 0;
+
+    while (1) {
+        /* Toggle LED state */
+        led_state = !led_state;
+        gpio_set_level(LED_GPIO, led_state);
+
+        /* Print only on OFF → ON */
+        if (led_state) {
+            int64_t time_us = esp_timer_get_time();
+            ESP_LOGI(TAG, "Hello World! Timestamp: %lld us", time_us);
+        }
+
+        /* 1 Hz blink → toggle every 500 ms */
+        vTaskDelay(pdMS_TO_TICKS(500));
+    }  // End while loop
+}  // End app_main
